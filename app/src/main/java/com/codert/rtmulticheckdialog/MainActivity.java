0b2464db5af8c5a083e00ee2ff37418c511dbc2b
CodeWrt <2437184862@qq.com>
2019-04-24 14:36:54 +0800

    更新示例！

--- app/src/main/java/com/codert/rtmulticheckdialog/MainActivity.java
+++ app/src/main/java/com/codert/rtmulticheckdialog/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codert.rtmulticheckdialog;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    //准备选项列表数据
     private void initData(){
         itemList = new ArrayList<>();
         for (int i = 1; i < 11; i++) {
@@ -40,7 +42,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initCustomDialog() {
-        RTMultiCheckDialog customDialog = new RTMultiCheckDialog(MainActivity.this,0.7,0.7)
+        Activity context = MainActivity.this;
+
+        //初始化dialog相关属性
+        //new RTMultiCheckDialog(context）为默认dialog宽高屏幕占比0.7
+        RTMultiCheckDialog customDialog = new RTMultiCheckDialog(context,0.7,0.7)
                 .setTitleText("请选择")
                 .setIcon(R.drawable.image)
                 .setConfirmText("确定")
@@ -48,18 +54,27 @@ public class MainActivity extends AppCompatActivity {
                 .setConfirmOnclicListener(new RTMultiCheckDialog.OnMultiCheckClickListener() {
                     @Override
                     public void onClick(RTMultiCheckDialog rtMultiCheckDialog) {
-                        Toast.makeText(MainActivity.this,"确定",Toast.LENGTH_SHORT).show();
+                        //点击确认事件
                         rtMultiCheckDialog.dismiss();
                     }
                 })
                 .setCancelOnclicListener(new RTMultiCheckDialog.OnMultiCheckClickListener() {
                     @Override
                     public void onClick(RTMultiCheckDialog rtMultiCheckDialog) {
-                        Toast.makeText(MainActivity.this,"取消",Toast.LENGTH_SHORT).show();
+                        //点击取消事件
                         rtMultiCheckDialog.dismiss();
                     }
                 })
                 .setItemNames(itemList);
+
+        //其他设置
+        //设置点击空白处是否关闭dialog，默认不关闭
+//        customDialog.setCancelInOutside(true);
+        //设置图标是否显示，默认显示
+//        customDialog.setIconShow(false);
+
+        //显示dialog
         customDialog.show();
+
     }
 }
